d521a9ce08205f514f0ae06e0dd57fa36d4790d2
Ege <egeege95@gmail.com>
2022-01-19 14:34:44 +0300

    initial commit

---
+++ controllers/tasks.js
@@ -0,0 +1,27 @@
+const getAllTasks = (req, res) => {
+  res.send("get all tasks");
+};
+
+const createTask = (req, res) => {
+  res.send("create a new task");
+};
+
+const getTask = (req, res) => {
+  res.send("get a single task");
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
